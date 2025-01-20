d679763d5bdf0fcbad2b5ccd51f3affc9f7acce0
Vishal Matsagar <vishalmatsagar0007@gmail.com>
2025-01-20 23:38:02 +0530

    add compenent which are required

--- src/App.js
+++ src/App.js
@@ -4,4 +4,17 @@ function App() {
   return <div className="App"></div>;
 }
 
+function Logo() {
+  return <div></div>;
+}
+function From() {
+  return <div></div>;
+}
+function Items() {
+  return <div></div>;
+}
+function Footer() {
+  return <div></div>;
+}
+
 export default App;
