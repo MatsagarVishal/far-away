cfb44c3dc369e2b6551d2b478653f1752fbcd9c4
Vishal Matsagar <vishalmatsagar0007@gmail.com>
2025-01-21 23:12:26 +0530

    Updated components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>far away</h1>
+      <Form />
+      <Items />
+      <Footer />
+    </div>
+  );
 }
 
-function Logo() {
-  return <div></div>;
+function Form() {
+  return (
+    <div>
+      <form>
+        <select>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+          <option value="" key="">
+            1
+          </option>
+        </select>
+        <input type="text" placeholder="Enter Item" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
-function From() {
-  return <div></div>;
-}
+
 function Items() {
-  return <div></div>;
+  return (
+    <div>
+      <span>item1</span>
+      <span>item1</span>
+      <span>item1</span>
+      <span>item1</span>
+    </div>
+  );
 }
 function Footer() {
-  return <div></div>;
+  return <div>you packed X out of X things</div>;
 }
 
 export default App;
